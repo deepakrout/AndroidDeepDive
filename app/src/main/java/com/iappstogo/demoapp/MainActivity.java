254a7a4da377c77bbd56cf16c8bca45c54f3779a
Deepak Rout <deepakrout@gmail.com>
2019-06-09 11:22:55 -0400

    Text Field added

--- app/src/main/java/com/iappstogo/demoapp/MainActivity.java
+++ app/src/main/java/com/iappstogo/demoapp/MainActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View buttonView) {
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+
+        Log.i("Info",  myTextField.getText().toString());
+
         Log.i("Info","Button Pressed!");
     }
 

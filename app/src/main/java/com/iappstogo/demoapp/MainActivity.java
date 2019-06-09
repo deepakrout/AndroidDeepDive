218a77f0c2386ee1e13b23ad890fe5430046ecbe
Deepak Rout <deepakrout@gmail.com>
2019-06-09 11:45:45 -0400

    Text Field added

--- app/src/main/java/com/iappstogo/demoapp/MainActivity.java
+++ app/src/main/java/com/iappstogo/demoapp/MainActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View buttonView) {
+
+        Toast.makeText(this, "Hi There!", Toast.LENGTH_LONG).show();
+
         EditText myTextField = (EditText) findViewById(R.id.myTextField);
 
         Log.i("Info",  myTextField.getText().toString());
